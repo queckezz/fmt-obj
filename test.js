1c53136e9a11a1b7ef77da5dbccf0f91eb8aeaa8
queckezz <fabian.eichenberger@gmail.com>
2017-02-17 10:34:39 +0100

    add tests

--- test.js
+++ test.js
@@ -16,8 +16,48 @@ const format = (obj, depth, offset) =>
   _format(obj, depth, identityFormatter, offset)
 
 test('literals', (t) => {
-  t.true(format({ obj: true }).includes('true'))
-  t.true(format({ obj: false }).includes('false'))
-  t.true(format({ obj: null }).includes('null'))
-  t.true(format({ obj: undefined }).includes('undefined'))
+  t.true(format({ val: true }).includes('true'))
+  t.true(format({ val: false }).includes('false'))
+  t.true(format({ val: null }).includes('null'))
+  t.true(format({ val: undefined }).includes('undefined'))
+})
+
+test('strings', (t) => {
+   t.true(format({ val: 'hello world' }).includes('"hello world"'))
+})
+
+test('numbers', (t) => {
+  t.true(format({ val: 12 }).includes('12'))
+  t.true(format({ val: 12e10 }).includes('120000000000'))
+  t.true(format({ val: -200 }).includes('-200'))
+  t.true(format({ val: Infinity }).includes('Infinity'))
+})
+
+test('functions', (t) => {
+  const normalFn = format({ helloWorld: () => {} })
+  t.true(normalFn.includes('[Function helloWorld]'))
+
+  const genFn = format({ helloWorld: function *() {} })
+  t.true(genFn.includes('[GeneratorFunction helloWorld]'))
+})
+
+test('depth', (t) => {
+  const actual = {
+    a: {
+      b: true,
+      c: {
+        d: true
+      }
+    }
+  }
+
+  const fmtNoDepth = format(actual, 0)
+  t.true(fmtNoDepth.includes('(collapsed)'))
+  t.false(fmtNoDepth.includes('b: true'))
+  t.false(fmtNoDepth.includes('d: true'))
+
+  const fmtOneLevel = format(actual, 1)
+  t.true(fmtOneLevel.includes('(collapsed)'))
+  t.true(fmtOneLevel.includes('b: true'))
+  t.false(fmtOneLevel.includes('d: true'))
 })
